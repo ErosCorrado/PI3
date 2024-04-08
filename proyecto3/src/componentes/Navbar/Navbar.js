3a6148679a46038e0ce4b980d0f5df87df92570f
ErosCorrado <ecorrado@udesa.edu.ar>
2024-04-08 16:49:23 -0300

    api popular

--- proyecto3/src/componentes/Navbar/Navbar.js
+++ proyecto3/src/componentes/Navbar/Navbar.js
@@ -8,7 +8,7 @@ function Navbar (props) {
             name: "Home",
             ruta: "/"
         }
-    
+      
     ];
 
     return (
--- proyecto3/src/screens/Home/Home.js
+++ proyecto3/src/screens/Home/Home.js
@@ -1,14 +1,40 @@
-import React, { Component } from 'react'
-
+import React, { Component } from 'react';
+import { options } from '../../Key/Key'
 
 class Home extends Component {
+    state = {
+        movies: [],
+    };
+
+    componentDidMount() {
+        this.fetchPopularMovies();
+    }
+
+    fetchPopularMovies = () => {
+        fetch('https://api.themoviedb.org/3/movie/popular', options)
+            .then(response => response.json())
+            .then(data => {
+                this.setState({ movies: data.results });
+            })
+            .catch(error => {
+                console.error('Error fetching popular movies:', error);
+            });
+    };
 
     render() {
+        const { movies } = this.state;
+
         return (
             <div>
                 <h1>Home</h1>
+                <h2>Popular Movies</h2>
+                
+                        {movies.map((movie) => (
+                            <li key={movie.id}>{movie.title}</li>
+                        ))}
             </div>
         );
     }
 }
+
 export default Home;
