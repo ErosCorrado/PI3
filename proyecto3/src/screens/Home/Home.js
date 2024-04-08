2667ed7033265aeff8bf70b363f82da975efbb90
ErosCorrado <ecorrado@udesa.edu.ar>
2024-04-08 17:08:17 -0300

    las dos apis

--- proyecto3/src/screens/Home/Home.js
+++ proyecto3/src/screens/Home/Home.js
@@ -4,25 +4,32 @@ import { options } from '../../Key/Key'
 class Home extends Component {
     state = {
         movies: [],
+        UpComing:[],
     };
 
     componentDidMount() {
-        this.fetchPopularMovies();
-    }
-
-    fetchPopularMovies = () => {
         fetch('https://api.themoviedb.org/3/movie/popular', options)
-            .then(response => response.json())
-            .then(data => {
-                this.setState({ movies: data.results });
-            })
-            .catch(error => {
-                console.error('Error fetching popular movies:', error);
-            });
-    };
+        .then(response => response.json())
+        .then(data => {this.setState({ movies: data.results.slice(0,5)});})
+        .catch(err => console.log(err))
 
+        fetch('https://api.themoviedb.org/3/movie/upcoming', options)
+        .then(response => response.json())
+        .then(data => {this.setState({ UpComing: data.results.slice(0,5)});})
+        .catch(err => console.log(err))
+    
+    
+    
+    
+    }
+    
+
+
+    
     render() {
         const { movies } = this.state;
+        const { UpComing } = this.state;
+
 
         return (
             <div>
@@ -32,6 +39,13 @@ class Home extends Component {
                         {movies.map((movie) => (
                             <li key={movie.id}>{movie.title}</li>
                         ))}
+                <h2>UpComing Movies</h2>
+                
+                        {UpComing.map((movie) => (
+                            <li key={movie.id}>{movie.title}</li>
+                        ))}
+                
+                        
             </div>
         );
     }
