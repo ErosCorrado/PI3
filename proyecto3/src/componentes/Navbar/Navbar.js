565e9ab643dc9ee2289bfacb565e99d6478f7eac
ErosCorrado <ecorrado@udesa.edu.ar>
2024-04-11 14:34:10 -0300

    cambios en el home

--- proyecto3/src/componentes/Navbar/Navbar.js
+++ proyecto3/src/componentes/Navbar/Navbar.js
@@ -1,44 +1,42 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 
-function Navbar (props) {
-
+function Navbar(props) {
     let navegacion = [
         {
             name: "Home",
             ruta: "/"
         },
         {
-          name: "Populares",
-          ruta: "/Populares"
+            name: "Populares",
+            ruta: "/Populares"
         },
         {
-          name: "Mejor Rankeadas",
-          ruta: "/TopRanked"
-        },
-      
+            name: "Mejor Rankeadas",
+            ruta: "/TopRanked"
+        }
     ];
 
     return (
         <div>
-      <nav >
-      <ul className='arriba'>
-        <li > 
-          <Link to ={navegacion[0].ruta} >
-            <img src="../../images/netflix.png" alt="" className="logo" />
-          </Link>
-        </li>
-              {
-                navegacion.map((elm) => 
-          <li className="itemlista">
-            <Link className="navlink" to={elm.ruta}>
-              {elm.name}
-            </Link>
-          </li> )
-              }
-      </ul>
-    </nav>
-    </div>
+            <nav>
+                <ul className='arriba'>
+                    <li>
+                        <Link to={navegacion[0].ruta}>
+                            <img src="../../images/netflix.png" alt="" className="logo" />
+                        </Link>
+                    </li>
+                    {navegacion.map((elm, index) => (
+                        <li key={index} className="itemlista">
+                            <Link className="navlink" to={elm.ruta}>
+                                {elm.name}
+                            </Link>
+                        </li>
+                    ))}
+                </ul>
+            </nav>
+        </div>
     );
 }
-export default Navbar
+
+export default Navbar;
--- proyecto3/src/screens/Home/Home.js
+++ proyecto3/src/screens/Home/Home.js
@@ -1,57 +1,8 @@
-import React, { Component } from 'react';
-import { options } from '../../Key/Key'
+import React from 'react';
+import Home from '../../componentes/Home/Home'; // Ruta relativa ajustada
 
-class Home extends Component {
-    constructor(props){
-        super(props)
-    this.state = {
-        movies: [],
-        TopRated:[],
-    }
-}
+const HomeScreen = () => {
+    return <Home />;
+};
 
-    componentDidMount() {
-        fetch('https://api.themoviedb.org/3/movie/popular', options)
-        .then(response => response.json())
-        .then(data => {this.setState({ movies: data.results.slice(0,5)});})
-        .catch(err => console.log(err))
-
-        fetch('https://api.themoviedb.org/3/movie/top_rated', options)
-        .then(response => response.json())
-        .then(data => {this.setState({ TopRated: data.results.slice(0,5)});})
-        .catch(err => console.log(err))
-    
-    
-    
-    
-    }
-    
-
-
-    
-    render() {
-        const { movies } = this.state;
-        const { TopRated } = this.state;
-
-
-        return (
-            <div>
-                <h1>Home</h1>
-                <h2>Peliculas Populares</h2>
-                
-                        {movies.map((movie) => (
-                            <li key={movie.id}>{movie.title}</li>
-                        ))}
-                <h2>Peliculas mejor rankeadas</h2>
-                
-                        {TopRated.map((movie) => (
-                            <li key={movie.id}>{movie.title}</li>
-                        ))}
-                
-                        
-            </div>
-        );
-    }
-}
-
-export default Home;
+export default HomeScreen;
