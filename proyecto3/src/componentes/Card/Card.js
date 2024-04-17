716c8ea97acc8490e868471bb9d0c28b9a6197d2
ErosCorrado <ecorrado@udesa.edu.ar>
2024-04-17 00:09:45 -0300

    correji el card

--- proyecto3/src/componentes/Card/Card.js
+++ proyecto3/src/componentes/Card/Card.js
@@ -1,17 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react'; // Importa Component de React
 import './Card.css'; 
 
-const Card = (props) => {
-    return (
-        <div className="card">
-            <div className="card-content">
-                <img src={props.image} alt={props.title} />
-                <h2>{props.title}</h2>
-                <p>{props.description}</p>
-                <button className="more">Agregar a fav</button>
+class Card extends Component { // Cambia el nombre de la clase a Card
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return  (
+            <div className="card">
+                <div className="card-content">
+                    <img src={this.props.image} alt={this.props.title} />
+                    <h2>{this.props.title}</h2>
+                    <p>{this.props.description}</p>
+                    <button className="more">Agregar a fav</button>
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
 
 export default Card;
