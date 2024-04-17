bc548b1b544dd74e411d4b8162d524afcee637c6
ErosCorrado <ecorrado@udesa.edu.ar>
2024-04-17 10:11:39 -0300

    ver mas y menos de las cards

--- proyecto3/src/componentes/Card/Card.js
+++ proyecto3/src/componentes/Card/Card.js
@@ -4,7 +4,32 @@ import './Card.css';
 class Card extends Component { // Cambia el nombre de la clase a Card
     constructor(props){
         super(props);
+        this.state= {
+          isSelected: false,
+          hiddenContent: true,
+          textBtn: 'Ver menos'
+        }
     }
+
+    seleccionarFicha(){
+        this.setState({
+          isSelected:true,
+        })
+      }
+
+      ocultarContenido(){
+        if(this.state.hiddenContent){
+          this.setState({
+            hiddenContent:false,
+            textBtn:'Ver menos'
+          })
+        } else {
+          this.setState({
+            hiddenContent:true,
+            textBtn:'Ver más'
+          })
+        }
+      }
 
     render(){
         return  (
@@ -12,7 +37,18 @@ class Card extends Component { // Cambia el nombre de la clase a Card
                 <div className="card-content">
                     <img src={this.props.image} alt={this.props.title} />
                     <h2>{this.props.title}</h2>
+                    {
+              this.state.hiddenContent 
+              ?
+              ""
+              :  
+              <div>
                     <p>{this.props.description}</p>
+              </div>
+              }
+              <button onClick={()=> this.ocultarContenido()}>
+              {this.state.textBtn}
+              </button>
                     <button className="more">Agregar a fav</button>
                 </div>
             </div>
